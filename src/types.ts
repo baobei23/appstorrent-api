ca1e2bce5f39a0eb9eef86384918ff7fec997fb5
baobei23 <reginaldsaja98@gmail.com>
2025-08-09 23:05:23 +0700

    init

---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+export interface Programs {
+  _id: string;
+  title: string;
+  version: string;
+  img: string;
+  category: string[];
+  url: string;
+  size: string;
+}
+
+export interface Software extends Omit<Programs, "url"> {
+  posters: string[];
+  developer: string;
+  interface_language: string;
+  activation: string;
+  compatibility: string;
+  architecture: string;
+  note: string;
+  downloads: DownloadLink[];
+  previous_version: DownloadLink[];
+  description: Paragraph[];
+  new_features: string;
+  official_site: string;
+  faq: Paragraph[];
+}
+
+export interface Paragraph {
+  title: string;
+  content: string;
+}
+
+export interface DownloadLink {
+  type: string;
+  link: string;
+}
